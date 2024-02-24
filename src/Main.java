02cd745cb4cc5064fe794d87a79cf66849a765a6
Mask04 <nandamask4@gmail.com>
2024-02-24 18:11:14 +0330

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i;
+        int j;
+        Scanner obj = new Scanner(System.in);
+        i = obj.nextInt();
+        j = obj.nextInt();
+
+    }
+}
